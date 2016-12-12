11330ef301ab720b1aae7467889f58a70531e55f
Anwesh43 <anweshthecool0@gmail.com>
2016-12-13 00:42:17 +0530

    Adding the demo

---
+++ app/src/main/java/com/anwesome/uiview/customchatdemo/MainActivity.java
@@ -0,0 +1,15 @@
+package com.anwesome.uiview.customchatdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.anwesome.uiview.leanchatfunctionality.LeanChatActivitiy;
+
+public class MainActivity extends LeanChatActivitiy {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
